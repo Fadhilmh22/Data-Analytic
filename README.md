d03f7691c2042535066f1a6daea4f2f7a8cd3510
Fadhiil Mursyid H <143087236+Fadhilmh22@users.noreply.github.com>
2024-12-04 15:54:03 +0000

    Perubahan readme dan lainya

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # Data-Analytic
+
+Aplikasi ini memungkinkan pengguna untuk melakukan Analisis Pengaruh Anggaran APBN Di Sektor Pendidikan Terhadap Angka Putus Sekolah. Aplikasi ini dibuat menggunakan **Streamlit** dan **Plotly** untuk visualisasi data.
+
+## Fitur Utama
+- **Filter Provinsi**: Pengguna dapat memilih provinsi yang ingin dianalisis melalui sidebar.
+- **Analisis Dropout**: Menampilkan statistik dropout untuk jenjang pendidikan tertentu dan rasio dropout terhadap jumlah murid.
+- **Visualisasi**: Menyediakan visualisasi data menggunakan grafik batang dan donut chart untuk memberikan gambaran yang jelas mengenai rasio dropout.
+
+## Cara Penggunaan
+
+### 1. Instalasi
+Pastikan Python sudah terinstal di perangkat Anda. Jika belum, Anda bisa mengunduhnya di [python.org](https://www.python.org/downloads/).
+
+1. Clone repository ini atau unduh file kode.
+2. Install semua dependensi yang dibutuhkan dengan perintah berikut di terminal atau command prompt:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+3. Jalankan aplikasi menggunakan perintah:
+
+    ```bash
+    streamlit run data.py
+    ```
+
+    Setelah itu, aplikasi akan berjalan di browser Anda.
+
+### 2. Menggunakan Filter Provinsi
+Pada sidebar, Anda akan menemukan opsi untuk memilih provinsi yang ingin dianalisis.
+
+1. **Pilih Provinsi**: Anda bisa memilih satu atau lebih provinsi yang diinginkan untuk analisis. Gunakan fitur **multiselect** untuk memilih provinsi yang sesuai.
+2. **Tanggal Analisis**: Tanggal analisis akan otomatis ditampilkan sesuai dengan hari Anda membuka aplikasi.
+
+Setelah memilih provinsi, aplikasi akan menampilkan data terkait provinsi yang dipilih beserta statistik terkait.
+
+### 3. Analisis Data Dropout
+Pada bagian utama aplikasi, Anda akan melihat statistik dropout yang terkait dengan jenjang pendidikan. Aplikasi ini menghitung dan menampilkan:
+
+- **Grafik Bar** yang menunjukkan jumlah dropout berdasarkan jenjang.
+- **Donut Chart** yang menampilkan rasio dropout terhadap total jumlah murid dalam bentuk grafik donat, dengan persentase yang dihitung secara otomatis.
+
+### 4. Tampilan Visualisasi
+Di bagian visualisasi, Anda dapat melihat grafik yang interaktif. Pengguna dapat memanfaatkan grafik ini untuk memahami sebaran data dropout dan rasio dropout dengan lebih mudah.
+
+## Teknologi yang Digunakan
+- **Streamlit**: Untuk membangun aplikasi web interaktif.
+- **Plotly**: Untuk visualisasi grafik donut chart.
+- **Pandas**: Untuk manipulasi data.
+- **Altair**: Untuk grafik tambahan (misalnya donut chart alternatif).
+  
+## Kontribusi
+Jika Anda ingin berkontribusi pada proyek ini, silakan lakukan langkah-langkah berikut:
+
+1. Fork repository ini.
+2. Buat branch baru untuk fitur atau perbaikan yang Anda kerjakan.
+3. Setelah selesai, kirimkan pull request untuk diskusi lebih lanjut.
+
+## Lisensi
+Proyek ini dilisensikan di bawah lisensi MIT. Lihat file [LICENSE](LICENSE) untuk informasi lebih lanjut.
--- data.py
+++ data.py
@@ -4,74 +4,77 @@ import plotly.graph_objects as go
 import folium
 from streamlit_folium import st_folium
 from datetime import date
+import matplotlib.pyplot as plt
+import altair as alt
 
 
 def display_map(df):
-    # Menambahkan dropdown untuk memilih jenjang pendidikan
     jenjang_options = ['SD', 'SMP', 'SMA', 'SMK']
-    selected_jenjang = st.selectbox("Pilih Jenjang Pendidikan:", jenjang_options)
-    
-    # Kolom yang relevan berdasarkan jenjang yang dipilih
+    selected_jenjang = st.selectbox(
+        "Pilih Jenjang Pendidikan:", jenjang_options)
+
     jumlah_sekolah_column = f"Jumlah Sekolah {selected_jenjang}"
-    
+
     if jumlah_sekolah_column not in df.columns:
         st.error(f"Data untuk jenjang {selected_jenjang} tidak tersedia.")
         return ""
-    
-    # Membuat peta dengan pusat Indonesia
-    map = folium.Map(location=[-6.1751, 106.8650], zoom_start=5, scrollWheelZoom=False, tiles='CartoDB positron')
-    
-    # Membaca GeoJSON dari file lokal
+
+    map = folium.Map(location=[-6.1751, 106.8650], zoom_start=5,
+                     scrollWheelZoom=False, tiles='CartoDB positron')
+
     geojson_url = 'indonesia-edit.geojson'
     df['PROVINSI'] = df['PROVINSI'].str.upper()
 
-    # Membuat Choropleth Map untuk peta Indonesia
     choropleth = folium.Choropleth(
         geo_data=geojson_url,
         data=df,
         columns=['PROVINSI', jumlah_sekolah_column],
         key_on='feature.properties.state',
-        fill_color='YlOrRd',  # Ganti dengan palet warna lain
+        fill_color='YlOrRd',  
         fill_opacity=0.7,
         line_opacity=0.2,
         legend_name=f"Jumlah Sekolah {selected_jenjang}"
     )
     choropleth.geojson.add_to(map)
 
-    # Menambahkan informasi tambahan pada setiap provinsi di GeoJSON
-    df_indexed = df.set_index('PROVINSI')  # Indeks menggunakan 'PROVINSI' untuk pencocokan
-    
+    df_indexed = df.set_index('PROVINSI')
+
     for feature in choropleth.geojson.data['features']:
-        state_name = feature['properties']['state'].upper()  # Nama provinsi di GeoJSON
+        state_name = feature['properties']['state'].upper()
         if state_name in df_indexed.index:
             jumlah_sekolah = df_indexed.loc[state_name, jumlah_sekolah_column]
             feature['properties']['population'] = f"Jumlah Sekolah {selected_jenjang}: {jumlah_sekolah}"
 
-    # Menambahkan tooltip pada GeoJSON untuk menampilkan informasi yang relevan
     choropleth.geojson.add_child(
-        folium.features.GeoJsonTooltip(['state', 'population'], labels=False)  # Menggunakan 'state' dan 'population' sebagai tooltip
+        folium.features.GeoJsonTooltip(['state', 'population'], labels=False)
     )
 
-    # Menampilkan peta di Streamlit
     st_map = st_folium(map, width=700, height=450)
 
-    # Menangani interaksi dengan peta, seperti klik pada provinsi
     state_name = ''
     if st_map.get('last_active_drawing'):
         state_name = st_map['last_active_drawing']['properties']['state']
-    
-    return state_name
 
+    return state_name
 
 # Load data
 data = pd.read_csv('Dataset_APBN_2023.csv')
 
 # Slide awal
 st.markdown("""
-<div style="background-color: #f0f8ff; padding: 20px; border-radius: 10px; margin-botton: 100px;">
-    <h1 style="color: #3333cc; text-align: center;">Dashboard Analisis Pengaruh Anggaran APBN Di Sektor Pendidikan Terhadap Angka Putus Sekolah Di Setiap Provinsi</h1>
-    <h2 style="text-align: center; color: #444;">Kelompok 3</h2>
-    <ul style="list-style: none; text-align: center; padding: 0; color: #333;">
+<div style="
+    background-color: #e6f7ff; 
+    padding: 30px; 
+    border-radius: 15px; 
+    margin-bottom: 50px; 
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);">    
+    <h1 style="color: #00509e; text-align: center; font-size: 2.5rem; font-family: 'Comic Sans ms', sans-serif; margin-bottom: 10px;">
+        Dashboard Analisis Pengaruh Anggaran APBN <br> Di Sektor Pendidikan Terhadap Angka Putus Sekolah
+    </h1>
+    <h2 style="text-align: center; color: #007acc; font-family: 'Comic Sans ms', sans-serif; margin-bottom: 30px;">
+        Kelompok 2
+    </h2>
+    <ul style="list-style: none; padding: 0; margin: 0; font-size: 1.2rem; color: #003f7d; font-family: 'Comic Sans ms', sans-serif; line-height: 2;">
         <li><b>Amelda Nur Azzuhra</b> (210414013)</li>
         <li><b>Haykal Hudiya Abiyyu</b> (210414020)</li>
         <li><b>Fadhiil Mursyid Habibi</b> (210414021)</li>
@@ -80,32 +83,41 @@ st.markdown("""
 </div>
 """, unsafe_allow_html=True)
 
-
-
 state_name = display_map(data)
 st.write(f'Provinsi yang dipilih: {state_name}')
 
-# Sidebar
-st.sidebar.image("school.png", width=250)  # Menampilkan logo dengan lebar 100px
-st.sidebar.title("Filter Data")
-selected_provinces = st.sidebar.multiselect("Pilih Provinsi", data['PROVINSI'].unique(), default=data['PROVINSI'].unique()[:5])
-analysis_date = date.today()
+from datetime import date
+import streamlit as st
 
-# Menampilkan tanggal sebagai teks di sidebar
+# Sidebar
+st.sidebar.image("school.png", width=250)
+st.sidebar.title("Filter Data")
+
+# Menambahkan instruksi di sidebar
+st.sidebar.markdown(
+    "Pilih provinsi yang ingin Anda analisis dari dropdown list di bawah ini. Anda dapat memilih satu atau beberapa provinsi untuk melihat data yang relevan."
+)
+
+# Multiselect untuk memilih provinsi
+selected_provinces = st.sidebar.multiselect(
+    "Pilih Provinsi", data['PROVINSI'].unique(), default=data['PROVINSI'].unique()[:1])
+
+# Menampilkan tanggal analisis
+analysis_date = date.today()
 st.sidebar.write(f"**Tanggal Analisis:** {analysis_date.strftime('%d-%m-%Y')}")
 
 if selected_provinces:
-    st.write(f"Analisis data untuk provinsi: **{', '.join(selected_provinces)}** pada tanggal **{analysis_date.strftime('%d-%m-%Y')}**")
+    st.write(
+        f"Analisis data untuk provinsi: **{', '.join(selected_provinces)}** pada tanggal **{analysis_date.strftime('%d-%m-%Y')}**")
 
 # Filter data berdasarkan provinsi yang dipilih
 filtered_data = data[data['PROVINSI'].isin(selected_provinces)]
 
-# Tabs
+
 tab1, tab2, tab3 = st.tabs(["Ringkasan", "Analisis Pendidikan", "Input Data"])
 
 # Tab 1: Ringkasan
 with tab1:
-    # Gaya CSS untuk membuat layout horizontal
     st.markdown("""
     <style>
     .metric-container {
@@ -134,12 +146,10 @@ with tab1:
     </style>
     """, unsafe_allow_html=True)
 
-    
-
     # Data Ringkasan
-    total_schools = int(filtered_data.iloc[:, 3:19:4].sum().sum())  # Jumlah total sekolah
-    total_students = int(filtered_data.iloc[:, 4:19:4].sum().sum())  # Jumlah total murid
-    total_budget = int(filtered_data['PAGU'].sum())  # Total anggaran
+    total_schools = int(filtered_data.iloc[:, 3:19:4].sum().sum())
+    total_students = int(filtered_data.iloc[:, 4:19:4].sum().sum())
+    total_budget = int(filtered_data['PAGU'].sum()) 
 
     # Layout Ringkasan
     st.markdown(f"""
@@ -159,7 +169,6 @@ with tab1:
     </div>
     """, unsafe_allow_html=True)
 
-    # Diagram Tambahan
     col1, col2 = st.columns(2)
 
     # Diagram 1: Sekolah per Jenjang
@@ -172,11 +181,11 @@ with tab1:
             filtered_data["Jumlah Sekolah SMA"].sum(),
             filtered_data["Jumlah Sekolah SMK"].sum(),
         ]
-        school_data = pd.DataFrame({"Jenjang": school_levels, "Jumlah Sekolah": school_counts})
+        school_data = pd.DataFrame(
+            {"Jenjang": school_levels, "Jumlah Sekolah": school_counts})
         st.bar_chart(school_data.set_index("Jenjang"))
 
         st.empty()
-
 
     # Diagram 2: Murid per Jenjang
     with col2:
@@ -187,11 +196,49 @@ with tab1:
             filtered_data["Jumlah Murid SMA"].sum(),
             filtered_data["Jumlah Murid SMK"].sum(),
         ]
-        student_data = pd.DataFrame({"Jenjang": school_levels, "Jumlah Murid": student_counts})
+        student_data = pd.DataFrame(
+            {"Jenjang": school_levels, "Jumlah Murid": student_counts})
         st.bar_chart(student_data.set_index("Jenjang"))
 
         st.empty()
 
+#         # Original Data (Semua Data Tanpa Filter)
+#         original_data = data.copy()
+
+#        # Memisahkan grafik Pagu ke bagian lain, di luar kolom
+# st.subheader("Pagu APBN 2023 per Provinsi")
+
+# # DataFrame Total Pagu
+# total_pagu_chart = pd.DataFrame({
+#     'Provinsi': original_data['PROVINSI'],
+#     'PAGU (Triliun)': original_data['PAGU'] / 1e12  # Konversi ke triliun
+# })
+
+# # Sort berdasarkan PAGU
+# total_pagu_chart = total_pagu_chart.sort_values(
+#     by="PAGU (Triliun)", ascending=True)
+
+# # Bar Chart menggunakan Altair
+# chart = alt.Chart(total_pagu_chart).mark_bar(color='#78B3CE').encode(
+#     x=alt.X('PAGU (Triliun):Q', title='PAGU (Triliun)',
+#             axis=alt.Axis(tickMinStep=5)),  # Sumbu X
+#     y=alt.Y('Provinsi:N', sort='-x', title='Provinsi'),  # Sumbu Y
+#     tooltip=['Provinsi', 'PAGU (Triliun)']  # Tooltip interaktif
+# ).properties(
+#     width=800,  # Lebar chart
+#     height=600  # Tinggi chart
+# ).configure_axis(
+#     grid=True,
+#     labelFontSize=12,
+#     titleFontSize=14
+# ).configure_title(
+#     fontSize=16,
+#     anchor='start',
+#     fontWeight='bold'
+# )
+
+# # Tampilkan Chart
+# st.altair_chart(chart, use_container_width=True)
 
 
 # Tab 2: Analisis Per Jenjang Pendidikan
@@ -199,8 +246,7 @@ with tab2:
     st.header("Analisis Pendidikan Per Jenjang")
     jenjang = st.selectbox("Pilih Jenjang", ["SD", "SMP", "SMA", "SMK"])
     st.write(f"Statistik untuk {jenjang}:")
-    
-    # Menampilkan data jenjang yang dipilih
+
     jenjang_columns = {
         "SD": ["Jumlah Sekolah SD", "Jumlah Kuota Murid SD", "Jumlah Murid SD", "Dropout SD"],
         "SMP": ["Jumlah Sekolah SMP", "Jumlah Kuota Murid SMP", "Jumlah Murid SMP", "Dropout SMP"],
@@ -208,11 +254,10 @@ with tab2:
         "SMK": ["Jumlah Sekolah SMK", "Jumlah Kuota Murid SMK", "Jumlah Murid SMK", "Dropout SMK"],
     }
     jenjang_data = filtered_data[jenjang_columns[jenjang]]
-    
-    # Grafik gabungan untuk semua data kecuali dropout
-    st.bar_chart(jenjang_data.drop(columns=["Dropout SD", "Dropout SMP", "Dropout SMA", "Dropout SMK"], errors='ignore').sum())
-    
-    # Tambahkan visualisasi terpisah untuk Dropout
+
+    st.bar_chart(jenjang_data.drop(columns=[
+                 "Dropout SD", "Dropout SMP", "Dropout SMA", "Dropout SMK"], errors='ignore').sum())
+
     st.write("Statistik Dropout")
     dropout_column = f"Dropout {jenjang}"
     if dropout_column in jenjang_data.columns:
@@ -221,44 +266,40 @@ with tab2:
     else:
         st.warning("Data dropout tidak ditemukan untuk jenjang ini.")
 
-    # Menampilkan Donut Chart untuk Dropout
     st.write("Rasio Dropout terhadap Total Murid")
     if dropout_column in jenjang_data.columns:
         total_students_jenjang = jenjang_data[f"Jumlah Murid {jenjang}"].sum()
         total_dropout_jenjang = jenjang_data[dropout_column].sum()
-        
-        # Hitung persentase dropout
-        dropout_percentage = (total_dropout_jenjang / total_students_jenjang) * 100 if total_students_jenjang > 0 else 0
 
-        # Data untuk Donut Chart hanya menunjukkan Dropout
+        dropout_percentage = (total_dropout_jenjang / total_students_jenjang) * \
+            100 if total_students_jenjang > 0 else 0
+
         labels = ["Dropout"]
         values = [total_dropout_jenjang]
 
-        # Membuat Donut Chart dengan Plotly
         fig = go.Figure(data=[go.Pie(
             labels=labels,
             values=values,
-            hole=0.7,  # Untuk menciptakan efek Donut
-            marker=dict(colors=["red"]),  # Warna Dropout merah
-            textinfo='none'  # Menghilangkan teks default (100%) pada chart
+            hole=0.7,  
+            marker=dict(colors=["red"]),  
+            textinfo='none'  
         )])
 
-        # Tambahkan teks di tengah donut
         fig.update_layout(
             annotations=[dict(
-                text=f"{dropout_percentage:.2f}%",  # Menampilkan persen dropout
-                x=0.5, 
-                y=0.5, 
-                font_size=20, 
-                showarrow=False, 
+                text=f"{dropout_percentage:.2f}%",
+                x=0.5,
+                y=0.5,
+                font_size=20,
+                showarrow=False,
                 font=dict(
-                    family="Comic Sans MS",  # Mengubah font menjadi Comic Sans
-                    size=20,  # Ukuran font
-                    color="red",  # Warna font merah
-                    style="italic"  # Membuat font miring
+                    family="Comic Sans MS",  
+                    size=20,  
+                    color="red",  
+                    style="italic" 
                 )
             )],
-            showlegend=False  # Sembunyikan legenda
+            showlegend=False 
         )
 
         st.plotly_chart(fig, use_container_width=True)
@@ -269,14 +310,15 @@ with tab2:
 # Tab 3: Input Data
 with tab3:
     st.header("Input Data Baru")
-    
-    # Radio button untuk memilih jenjang pendidikan
-    school_level = st.radio("Pilih Tingkatan Sekolah", ["SD", "SMP", "SMA", "SMK"])
-    
-    # Input untuk jumlah murid, tingkat dropout
-    additional_students = st.number_input("Tambahkan Jumlah Murid", min_value=0, step=1)
-    additional_dropout = st.number_input("Tambahkan Tingkat Dropout", min_value=0, step=1)
-    
+
+    school_level = st.radio("Pilih Tingkatan Sekolah", [
+                            "SD", "SMP", "SMA", "SMK"])
+
+    additional_students = st.number_input(
+        "Tambahkan Jumlah Murid", min_value=0, step=1)
+    additional_dropout = st.number_input(
+        "Tambahkan Tingkat Dropout", min_value=0, step=1)
+
     if st.button("Simpan Data"):
         level_columns = {
             "SD": ("Jumlah Murid SD", "Dropout SD"),
@@ -286,18 +328,19 @@ with tab3:
         }
         student_col, dropout_col = level_columns[school_level]
         for province in selected_provinces:
-            data.loc[data['PROVINSI'] == province, student_col] += additional_students
-            data.loc[data['PROVINSI'] == province, dropout_col] += additional_dropout
-        
+            data.loc[data['PROVINSI'] == province,
+                     student_col] += additional_students
+            data.loc[data['PROVINSI'] == province,
+                     dropout_col] += additional_dropout
+
         data.to_csv('Dataset_APBN_2023.csv', index=False)
-        st.success(f"Data berhasil diperbarui untuk jenjang {school_level}! Ditambahkan pada provinsi: {', '.join(selected_provinces)}")
-        st.write(f"{student_col} +{additional_students}, {dropout_col} +{additional_dropout}")
+        st.success(
+            f"Data berhasil diperbarui untuk jenjang {school_level}! Ditambahkan pada provinsi: {', '.join(selected_provinces)}")
+        st.write(
+            f"{student_col} +{additional_students}, {dropout_col} +{additional_dropout}")
 
 # Expander for details
 with st.expander("Detail Provinsi"):
-    # Mengganti nama index yang lama menjadi "Index"
     filtered_data.index.name = "Index"
-    
+
     st.write(filtered_data)
-
-
